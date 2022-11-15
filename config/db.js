f8e3ed734ca542713e2c4b4574a984529c47a13b
AndreBE99 <andrebetan.iteso@gmail.com>
2022-11-15 15:54:27 -0600

    google auth implementado (falta validar)

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL);
+// mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect('mongodb+srv://andre:andre@pae2022.cy4rx6j.mongodb.net/?retryWrites=true&w=majority');
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Database connected'));
