4e8289116018ec2e662b251db5366a975a9e71fa
AndreBE99 <andrebetan.iteso@gmail.com>
2022-11-29 18:06:09 -0600

    Socket.io implemented

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-// mongoose.connect(process.env.DATABASE_URL);
-mongoose.connect('mongodb+srv://andre:andre@pae2022.cy4rx6j.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Database connected'));
